85b07f61463cab8d518796c26c2fe4e44e79a8e0
sm.steve.moretz <sm.steve.moretz@gmail.com>
2021-12-10 13:41:28 +0330

    fa

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1afa811878d3ecb24f63f5d603cea377.js"
+  "/precache-manifest.f2ff1dbd56daa73d1eb724b4af90a217.js"
 );
 
 self.addEventListener('message', (event) => {
